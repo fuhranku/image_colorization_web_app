a14d6f15e43c63b358b0ac0078ca98bf888c3aa7
Jose Tirado <jose_enrique_29@hotmail.com>
2020-11-23 22:31:00 -0400

    creating test function

---
+++ PDI_project/computer_vision/test.py
@@ -0,0 +1,6 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def log_print():
+    logger.error("arre loco")
--- PDI_project/views.py
+++ PDI_project/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from .computer_vision.test import log_print
+
 # Create your views here.
 def index(request):
+    log_print()
     # Renderiza la plantilla HTML index.html con los datos en la variable contexto
     return render(
         request,
