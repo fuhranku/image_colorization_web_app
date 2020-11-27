4e8d925010a6e6b42296adf7fdb01dc9d99ac072
fuhranku <frankponte95@gmail.com>
2020-11-26 21:01:22 -0400

    UPDATE::Colorization alpha version working

--- PDI_project/computer_vision/imageProcesser.py
+++ PDI_project/computer_vision/imageProcesser.py
@@ -5,8 +5,6 @@ from keras.layers import Activation, Dense, Dropout, Flatten
 from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
-from skimage.io import imsave, imshow
 from PIL import Image 
 import numpy as np
 import os
@@ -18,47 +16,56 @@ from django.core.files.base import ContentFile
 from django.conf import settings
 from django.contrib.staticfiles import finders
 
-
-logger = logging.getLogger(__name__)
-
 def LogPrint(object):
+    logger = logging.getLogger(__name__)
     logger.error(": "+str(object))
 
 def ProcessImage(request):
     if request is not None:
         # Grab uploaded image
-        image = GrabImage(stream=request)
+        image = LoadImage(stream=request)
         # Modify Image
-
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
-
-        image = np.asarray(image, dtype=float)
-
-        X = 1.0/255*image[:,:,0]
-        X = X.reshape(1, 400, 400, 1)
-
-        model = tf.keras.models.load_model(finders.find('trained_models/my_model'))
-        output = model.predict(X)    
-        output *= 128
-        # Output colorizations
-        cur = np.zeros((400, 400, 3))
-        cur[:,:,0] = X[0][:,:,0]
-        cur[:,:,1:] = output[0]
-        
-        # lab2rgb(cur)
-        output = cur.astype("uint8")
-        output = cv2.cvtColor(output,cv2.COLOR_Lab2RGB)
-        #output = output * 255
-        LogPrint(output)
-        #output = Image.fromarray(cur.astype(np.uint8))
-        #output = cv2.cvtColor(output, cv2.COLOR_Lab2RGB)
-
+        image = Colorize(image)
         # Save image on disk
-        imageURL = SaveImage(output, request.name)
+        imageURL = SaveImage(image, request.name)
         # Return processed image URL
         return imageURL
 
-def GrabImage(path=None, stream=None, url=None):
+def Colorize(image):
+    # Get dimensions of image
+    (H,W) = image.shape[:2]
+    # Normalize image to handle variations in intensity
+    image = (image * 1.0 / 255).astype(np.float32)
+    # Convert to Lab color space and grab channel L
+    image = cv2.cvtColor(image,cv2.COLOR_RGB2Lab)
+    # Get L channel from image
+    X = image[:,:,0]
+    # Resize image to network input size
+    X = X.reshape(1,W,H,1)
+    # Load trained model
+    model = tf.keras.models.load_model(finders.find('trained_models/my_model'))
+    # Colorize Image with CNN
+    output = model.predict(X)
+    # Denormalize ab channels from L * a * b
+    output *= 128
+    # Putting all together
+    colorizedImage = np.zeros((W,H,3))
+    # Get L Channel from prepros image
+    colorizedImage[:,:,0] = X[0][:,:,0]
+    # Get ab Channel from output image
+    colorizedImage[:,:,1:] = output[0]
+    # Convert to float32
+    colorizedImage = colorizedImage.astype(np.float32)
+    # Convert to RGB
+    colorizedImage = cv2.cvtColor(colorizedImage,cv2.COLOR_Lab2RGB)
+    # Denormalize values
+    colorizedImage = colorizedImage * 255
+    # Cast values to unsigned int
+    colorizedImage = colorizedImage.astype(np.uint8)
+
+    return colorizedImage
+
+def LoadImage(path=None, stream=None, url=None):
     # if the path is not None, then load the image from disk
     if path is not None:
         image = cv2.imread(path)
@@ -74,14 +81,17 @@ def GrabImage(path=None, stream=None, url=None):
         # convert the image to a NumPy array and then read it into
         # OpenCV format
         image = np.asarray(bytearray(data), dtype="uint8")
-        # Decode into bgr 
+        # Decode into BGR 
         image = cv2.imdecode(image, cv2.IMREAD_COLOR) 
-        # Convert into rgb 
+        # Convert into RGB 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     # return the image
     return image
 
 def SaveImage(image, filename=None):
+    # Convert back into BGR
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     # Encode back
     ret, buf = cv2.imencode('.jpg', image)
     content = ContentFile(buf.tobytes())
@@ -91,3 +101,4 @@ def SaveImage(image, filename=None):
     imgModel.image.save(filename,content)
     # Return local image URL
     return imgModel.image.url
+
--- PDI_project/models.py
+++ PDI_project/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ImageModel(models.Model):
-    title = models.CharField(max_length=100, default="output.jpg")  # this field does not use in your project
-    image = models.FileField(upload_to='img/')
+    title  = models.CharField(max_length=100, default="output.jpg")  # this field does not use in your project
+    image  = models.FileField(upload_to='img/')
+    width  = models.IntegerField(null=True)
+    height = models.IntegerField(null=True)
